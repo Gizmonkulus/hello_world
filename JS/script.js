2153cbff445035ca1471728e8e91aa350360ceb0
StMusin <stmusin89@gmail.com>
2021-04-06 20:58:16 +0300

    1st JS lesson

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,37 @@
 "use strict";
 //console.log();
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 'over9k');
+
+
+
+
+const personalMovieDB = {
+    count: numberOfFilms,        
+    movies:{},
+    actors: null,
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?'),
+    b = prompt('На сколько оцените его?'),
+    c = prompt('Один из последних просмотренных фильмов?'),
+    d = prompt('На сколько оцените его?');
+    
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+// 3) Задайте пользователю по два раза вопросы:
+//     - 'Один из последних просмотренных фильмов?'
+//     - 'На сколько оцените его?'
+// Ответы стоит поместить в отдельные переменные
+// Записать ответы в объект movies в формате: 
+//     movies: {
+//         'logan': '8.1'
+//     }
